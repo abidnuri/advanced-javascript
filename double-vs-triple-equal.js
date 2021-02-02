330873c5a8cb6a4d8085352fbac1b3539326d24e
Abid Nuri <mdabidnuri@gmail.com>
2021-02-02 13:10:53 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,19 @@
+//double equal == it's checking just the value not the type
+const first = 0;
+const second = false;
+if (first == second) {
+    console.log('Condition is true.')
+}
+else {
+    console.log('Condition is false.')
+}
+
+//triple equal === it's checking strictly the value & the type also.
+const third = 0;
+const fourth = false;
+if (third === fourth) {
+    console.log('Condition is true.')
+}
+else {
+    console.log('Condition is false.')
+}
